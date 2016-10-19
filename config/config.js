39041a76e632c93689606721714d72b139f6dfaf
leticia <lei.8293@hotmail.com>
2016-10-19 18:46:49 -0500

    texto modular

---
+++ config/config.js
@@ -0,0 +1,11 @@
+//exportar por elemento
+//exports.numero0=10;
+//exports.numero1=11;// se puede ver desde otro archivo
+//exportar por bloque
+//module.exports=10;
+
+//exportar objeto de configuracion 
+module.exports= {
+    'IP': process.env.IP || '127.0.0.1',
+    'PORT':process.env.IP || 3000,
+};
